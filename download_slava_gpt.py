9ebe4e6f7b2d71ac090cf80896ef994750a4246f
elderberry17 <bezgin14a@mail.ru>
2023-08-25 13:40:59 +0300

    comments

--- download_slava_gpt.py
+++ download_slava_gpt.py
@@ -1,6 +1,7 @@
 import gdown
 
 if __name__ == '__main__':
+    # url for model in .zip archive
     url =
     output_file = "slavagpt.zip"
     gdown.download(url, output_file, quiet=False, fuzzy=True)
